4f457ac4a300bd2e56679cb1d1fa87d653ab5ff2
Zok0ta <65073455+Zok0ta@users.noreply.github.com>
2020-07-11 23:18:53 +0200

    Update ambassadeur.js

--- .gitignore/commande/ambassadeur.js
+++ .gitignore/commande/ambassadeur.js
@@ -6,14 +6,16 @@ module.exports.run = async (bot, message, args) => {
     message.delete();
     let aMember = message.guild.member(message.author);
     let role = message.guild.roles.cache.find(r => r.name == "Ambassadeur");
-    if(aMember.roles.cache.has(role.id)) return message.channel.send("Vous avez déja le role ambassadeur")
+    if(aMember.roles.cache.has(role.id)) return message.channel.send("Vous etes déja inscrit en tant qu'ambassadeur")
     else {
     await aMember.roles.add(role.id).catch(console.error);
-    let légat = message.guild.roles.cache.find(r => r.name == "Ambassadeur");
-    // Logs: Tribun: <@&671882885527240719>
-    message.channel.send("Le rôle t'a été affecté  " +`${aMember}, le `+ `${légat}` + "ou le " + `${légat}`+" va te prendre en charge ^^ v2");
+    let légat = message.guild.roles.cache.find(r => r.name == "Légat");
+    let Tribun = message.guild.roles.cache.find(r => r.name == "Tribun");
+    message.channel.send("Le rôle t'a été affecté  " +`${aMember}, le `+ `${légat}` + " ou le " + `${Tribun}`+" va te prendre en charge ^^");
     }
 }
 
 module.exports.help = {
-    name : "ambassadeur"
+    name : "ambassadeur",
+    name : "Ambassadeur"
+}
