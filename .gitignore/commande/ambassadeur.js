79f0f4309abcf8e7deccc6f990f471f17e8f15af
Zok0ta <65073455+Zok0ta@users.noreply.github.com>
2020-07-11 21:52:14 +0200

    Update ambassadeur.js

--- .gitignore/commande/ambassadeur.js
+++ .gitignore/commande/ambassadeur.js
@@ -6,13 +6,14 @@ module.exports.run = async (bot, message, args) => {
     message.delete();
     let aMember = message.guild.member(message.author);
     let role = message.guild.roles.cache.find(r => r.name == "Ambassadeur");
-    if(aMember.roles.cache.has(role.id)) return message.channel.send("Vous avez déja ce role")
+    if(aMember.roles.cache.has(role.id)) return message.channel.send("Vous avez déja le role ambassadeur")
     else {
     await aMember.roles.add(role.id).catch(console.error);
-    message.channel.send("Le rôle t'a été affecté  " +`${aMember}, le @Légat ou le @Tribun va te prendre en charge ^^`);
+    // Logs: légat: <@&671882659689136168>
+    // Logs: Tribun: <@&671882885527240719>
+    message.channel.send("Le rôle t'a été affecté  " +`${aMember}, le `+ `${légat}` + "ou le " + `${légat}`+" va te prendre en charge ^^ v2");
     }
 }
 
 module.exports.help = {
     name : "ambassadeur"
-}
