7d6158baec09d23d84e6a4091ad0e3d25f15992b
Zok0ta <65073455+Zok0ta@users.noreply.github.com>
2020-07-11 23:07:37 +0200

    Update gt.js

--- .gitignore/commande/gt.js
+++ .gitignore/commande/gt.js
@@ -1,18 +1,19 @@
 const Discord = require("discord.js")
-const colours = require("../colours.json")
 
 module.exports.run = async (bot, message, args) => {
     let messageArray = message.content.split(" ");
+    let Givrelame = message.guild.roles.cache.find(r => r.name == "Givrelame");
     if(messageArray.length == 4){
         message.delete()
-        message.channel.send("**@Givrelame, le règne du sang est programmé !**")
+        message.channel.send(`${Givrelame}` + ", le règne du sang est programmé !**")
         let aEmbed = new Discord.MessageEmbed()
-            .setColor(colours.blue_dark)
-            .setTitle("Guerre territorial du "+messageArray[1]+" "+messageArray[2]+" "+messageArray[3])
-            .addField("* - *","Guerriers de Givrelame ! Voici le sondage GT du "+messageArray[1]+" "+messageArray[2]+" "+messageArray[3]+" !")
-            .addField("* - *","Comme d'hab on répond par le bon émot !")
-            .addField("* - *","Présent :white_check_mark:  Absent :x:  Retard :rabbit:  Incertains :question:")
-            
+        .setTitle("Guerre territorial du "+messageArray[1]+" "+messageArray[2]+" "+messageArray[3])
+        .setAuthor('Givrelame', 'https://cdn.discordapp.com/attachments/689447634355617868/728997445060657192/2.png')            
+        .setColor('RANDOM')
+        .addFields(
+            { name: 'Guerriers de la Givrelame !', value: "Comme d'habitude on répond par le bon émot !" , inline: true },
+            { name: "Comment s'incrire" , value: "Présent :white_check_mark:  Absent :x:  Retard :rabbit:  Incertains :question:", inline: true },
+        )
             
         message.channel.send(aEmbed)
     } 
@@ -22,5 +23,7 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name : "gt"
+    name : "gt",
+    name : "Gt",
+    name : "GT"
 }
