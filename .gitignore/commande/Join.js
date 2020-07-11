2ecb73d69074aa1a97191db4dae9a881de412c09
Zok0ta <65073455+Zok0ta@users.noreply.github.com>
2020-07-11 23:08:59 +0200

    Update Join.js

--- .gitignore/commande/Join.js
+++ .gitignore/commande/Join.js
@@ -6,14 +6,16 @@ module.exports.run = async (bot, message, args) => {
     message.delete();
     let aMember = message.guild.member(message.author);
     let role = message.guild.roles.cache.find(r => r.name == "Recrutement");
+    let Recruteur = message.guild.roles.cache.find(r => r.name == "Recruteur");
     if(aMember.roles.cache.has(role.id)) return message.channel.send("Vous avez déja ce role")
     else {
     await aMember.roles.add(role.id).catch(console.error);
     
-    message.channel.send("Le rôle t'a été affecté " +`${aMember}, un @Moderateur va te prendre en charge ^^`);
+    message.channel.send("Le rôle t'a été affecté " +`${aMember}, un `+ `${Recruteur} va te prendre en charge ^^`);
     }
 }
 
 module.exports.help = {
+    name : "join",
     name : "Join"
 }
